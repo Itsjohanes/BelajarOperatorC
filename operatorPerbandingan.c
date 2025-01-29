20bf5d558c07aa270ae25b1d28c8e4ead6955e92
Johannes Alexander Putra <71767003+Itsjohanes@users.noreply.github.com>
2025-01-29 18:51:40 +0700

    Add files via upload

---
+++ operatorPerbandingan.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	int x = 5;
+	int y = 3;
+	//1 itu true 0 itu false
+	printf("%d\n", x > y);
+	printf("%d\n", x >= y);
+	printf("%d\n", x < y);
+	printf("%d\n", x <= y);
+	printf("%d\n", x == y);
+}
